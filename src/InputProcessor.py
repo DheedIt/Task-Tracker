10a856bc2e79c588b09ffde1ba19d9973f0b278a
DheedIt <zhulanoff.ilia@yandex.ru>
2026-04-04 16:18:06 +0400

    Промежуточные изменения

--- src/InputProcessor.py
+++ src/InputProcessor.py
@@ -1,13 +1,37 @@
 import sys
+from ent import Task
 def inputProcessor(args):
  options = {
-  "add": lambda: 
-  "update"
-  "delete"
-  "list"
-  "mark-in-progress"
-  "mark-done"
+  "add": lambda: add(args[1])
+  # "update"
+  # "delete"
+  # "list"
+  # "mark-in-progress"
+  # "mark-done"
  }
+ handler = options.get(args[0], lambda: print('Балбес'))
+ handler()
 
-def 
+def add(name):
+ res = Task(1,name)
+ print(res.id,)
+ print(res.escription)
+ return res
+ 
+
+def update(name):
+ return Task(1,name)
+
+def delete(name):
+ return Task(1,name)
+
+def list(name):
+ return Task(1,name)
+
+def markIP(name):
+ return Task(1,name)
+
+def markD(name):
+ return Task(1,name)
+
 
--- src/ent.py
+++ src/ent.py
@@ -1,3 +1,6 @@
+from sqlite3 import Date
+
+
 class Task:  
     def __init__(self,id,escription,status,createdAt,updateAt):
         self.id = id
@@ -5,3 +8,10 @@ class Task:
         self.status = status
         self.createdAt = createdAt
         self.updateAt = updateAt
+        
+    def __init__(self,id,description):
+        self.id = id
+        self.escription = description
+        self.status = ""
+        self.createdAt = Date.today()
+        self.updateAt = Date.today()
